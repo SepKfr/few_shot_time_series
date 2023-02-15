64fae7e1fab10c075354cda472f1b8b0099715c8
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-02-15 11:59:46 -0500

    test for conv_attn

--- train.py
+++ train.py
@@ -228,8 +228,8 @@ def main():
     parser.add_argument("--model_name", type=str, default="conv_attn")
     parser.add_argument("--exp_name", type=str, default='traffic')
     parser.add_argument("--cuda", type=str, default="cuda:0")
-    parser.add_argument("--seed", type=int, default=1234)
-    parser.add_argument("--n_trials", type=int, default=3)
+    parser.add_argument("--seed", type=int, default=1899)
+    parser.add_argument("--n_trials", type=int, default=50)
     parser.add_argument("--num_epochs", type=int, default=50)
 
     args = parser.parse_args()
