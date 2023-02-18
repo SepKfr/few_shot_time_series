08db765864e0c7f97eeb077693a73541f2d2feb4
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-02-18 10:34:37 -0500

    batch_size 256

--- forecasting_models/ATA.py
+++ forecasting_models/ATA.py
@@ -44,7 +44,8 @@ class ATA(nn.Module):
 
         self.few_shot = few_shot
         if self.few_shot:
-            self.clustering = Clustering(device=device, d_model=d_k*h, d_k=d_k)
+            self.clustering = Clustering(device=device, d_model=d_k*h)
+            self.layer_norm = nn.LayerNorm(d_k, elementwise_affine=False, device=device)
         self.factor = 1
 
     def forward(self, Q, K, V, attn_mask):
@@ -75,11 +76,12 @@ class ATA(nn.Module):
         K = self.proj_back_k(K)
 
         if self.few_shot:
-            K_out, loss = self.clustering(K)
-            scores = torch.einsum('bhqd,bhkd->bhqk', Q, K_out) / np.sqrt(self.d_k)
+            context_clustering, loss = self.clustering(Q, K, V)
+            scores = torch.einsum('bhqd,bhkd->bhqk', Q, K) / np.sqrt(self.d_k)
             attn = torch.softmax(scores, -1)
             context = torch.einsum('bhqk,bhkd->bhqd', attn, V)
-            return context, attn, loss
+            context_final = self.layer_norm(context + context_clustering)
+            return context_final, attn, loss
 
         else:
             scores = torch.einsum('bhqd,bhkd->bhqk', Q, K) / np.sqrt(self.d_k)
--- modules/clustering.py
+++ modules/clustering.py
@@ -8,36 +8,45 @@ import numpy as np
 
 
 class Clustering(nn.Module):
-    def __init__(self, *, device, num_clusters=5, d_model, d_k):
+    def __init__(self, *, device, num_clusters=5, d_model):
         super(Clustering, self).__init__()
 
         self.device = device
         self.num_clusters = num_clusters
 
-        self.proj_to_cluster_k = nn.Sequential(nn.Linear(d_model, num_clusters,
+        self.proj_to_cluster_k = nn.Sequential(nn.Conv2d(d_model, num_clusters,
+                                                         kernel_size=(1, 3),
+                                                         padding=(0, 1),
                                                          device=self.device),
-                                                         nn.ReLU())
-        self.proj_back_to_cluster_k = nn.Sequential(nn.Linear(num_clusters, d_model,
-                                                              device=self.device),
-                                                              nn.ReLU())
+                                                         nn.BatchNorm2d(num_clusters),
+                                                         nn.Softmax(dim=-1))
+        self.proj_back_to_cluster_k = nn.Sequential(nn.Conv2d(num_clusters, d_model,
+                                                              device=self.device,
+                                                              kernel_size=(1, 3),
+                                                              padding=(0, 1)),
+                                                              nn.BatchNorm2d(d_model),
+                                                              nn.Softmax(dim=-1))
         self.cluster_k_proj = nn.Linear(num_clusters, num_clusters, device=self.device)
         self.cluster_q_proj = nn.Linear(num_clusters, num_clusters, device=self.device)
 
-        self.layer_norm = nn.LayerNorm(d_k, device=self.device, elementwise_affine=False)
         self.cross_entropy = nn.CrossEntropyLoss()
 
-    def forward(self, K):
+    def forward(self, Q, K, V):
 
-        b, h, l_k, d_k = K.shape
-        unfolding = int(b/4)
+        b, h, l, d_k = Q.shape
 
-        padding = torch.zeros(unfolding, h, l_k, d_k, device=self.device)
+        K = nn.MaxPool1d(kernel_size=9, padding=int((9-1)/2))(K.reshape(b, d_k*h, -1)).reshape(b, h, -1, d_k)
+        V = nn.MaxPool1d(kernel_size=9, padding=int((9-1)/2))(V.reshape(b, d_k*h, -1)).reshape(b, h, -1, d_k)
+
+        l_k = K.shape[2]
+
+        padding = torch.zeros(int(b/2), h, l_k, d_k, device=self.device)
         K_padded = torch.cat([padding, K[1:]])
-        K_unfold = K_padded.unfold(0, unfolding, 1)
+        K_unfold = K_padded.unfold(0, int(b/2), 1)
 
-        K_unfold = K_unfold.reshape(b, l_k, -1, d_k*h)
+        K_unfold = K_unfold.reshape(b, d_k*h, l_k, -1)
 
-        cluster_k_p = self.proj_to_cluster_k(K_unfold)
+        cluster_k_p = self.proj_to_cluster_k(K_unfold).permute(0, 3, 2, 1)
 
         cluster_k = self.cluster_k_proj(cluster_k_p)
         cluster_q = self.cluster_q_proj(cluster_k_p)
@@ -57,14 +66,16 @@ class Clustering(nn.Module):
 
         ind_clusters = ind_clusters.unsqueeze(-1).repeat(1, 1, 1, self.num_clusters)
 
-        cluster_centers = [torch.mean(cluster_q.clone().masked_fill_((ind_clusters == i), 0.0), dim=2)
+        cluster_centers = [torch.mean(cluster_q.clone().masked_fill_((ind_clusters == i), 0.0), dim=1)
                            for i in range(self.num_clusters)]
 
         cluster_center = torch.stack(cluster_centers)
 
-        cluster_center = self.proj_back_to_cluster_k(cluster_center).reshape(b, h, self.num_clusters, l_k, d_k)
-        cluster_center = torch.mean(cluster_center, dim=2)
+        cluster_center = self.proj_back_to_cluster_k(cluster_center.permute(0, 3, 1, 2)).reshape(b, h, -1, l_k, d_k)
+        scores_center = torch.einsum('bhqd, bhckd -> bhqk', Q, cluster_center)
 
-        K_out = self.layer_norm(K + cluster_center)
+        attn = torch.softmax(scores_center, -1)
 
-        return K_out, loss
+        context = torch.einsum('bhqk, bhkd -> bhqd', attn, V)
+
+        return context, loss
