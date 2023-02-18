c354252b75bc021742e2b5f00ef31f6ad54f1c65
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-02-18 09:25:28 -0500

    batch_size 256

--- modules/clustering.py
+++ modules/clustering.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 class Clustering(nn.Module):
-    def __init__(self, *, device, num_clusters=5, d_model):
+    def __init__(self, *, device, num_clusters=20, d_model):
         super(Clustering, self).__init__()
 
         self.device = device
