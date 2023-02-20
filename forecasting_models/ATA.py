1775627c0bc504d43175e7394b9d051c6bd3c8a2
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-02-20 10:15:37 -0500

    adding w1 and w2

--- forecasting_models/ATA.py
+++ forecasting_models/ATA.py
@@ -46,6 +46,8 @@ class ATA(nn.Module):
         if self.few_shot:
             self.clustering = Clustering(device=device, d_model=d_k*h)
             self.layer_norm = nn.LayerNorm(d_k, elementwise_affine=False, device=device)
+            self.w1 = nn.Linear(d_k, d_k, device=device)
+            self.w2 = nn.Linear(d_k, d_k, device=device)
         self.factor = 1
 
     def forward(self, Q, K, V, attn_mask):
@@ -79,8 +81,10 @@ class ATA(nn.Module):
             context_clustering, loss = self.clustering(Q, K, V)
             scores = torch.einsum('bhqd,bhkd->bhqk', Q, K) / np.sqrt(self.d_k)
             attn = torch.softmax(scores, -1)
-            context = self.layer_norm(torch.einsum('bhqk,bhkd->bhqd', attn, V) + context_clustering)
-            return context, attn, loss
+            context = torch.einsum('bhqk,bhkd->bhqd', attn, V)
+            context_final = self.layer_norm(self.w1(context) + self.w2(context_clustering))
+
+            return context_final, attn, loss
 
         else:
             scores = torch.einsum('bhqd,bhkd->bhqk', Q, K) / np.sqrt(self.d_k)
