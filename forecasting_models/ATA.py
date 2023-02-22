b5f8e27a17d52ea6fffba51eadda598c27db77b7
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-02-22 07:47:33 -0500

    adding covid

--- forecasting_models/ATA.py
+++ forecasting_models/ATA.py
@@ -47,10 +47,8 @@ class ATA(nn.Module):
         if self.few_shot:
             self.clustering = Clustering(device=device, d_model=d_k*h)
             self.layer_norm = nn.LayerNorm(d_k, elementwise_affine=False, device=device)
-            self.w1 = PoswiseFeedForwardNet(
-                d_model=self.d_k, d_ff=self.d_k*4, seed=seed).to(self.device)
-            self.w2 = PoswiseFeedForwardNet(
-                d_model=self.d_k, d_ff=self.d_k*4, seed=seed).to(self.device)
+            self.w1 = nn.Sequential(nn.Linear(d_k, d_k, device=self.device),
+                                    nn.GELU())
         self.factor = 1
 
     def forward(self, Q, K, V, attn_mask):
@@ -85,7 +83,7 @@ class ATA(nn.Module):
             scores = torch.einsum('bhqd,bhkd->bhqk', Q, K) / np.sqrt(self.d_k)
             attn = torch.softmax(scores, -1)
             context = torch.einsum('bhqk,bhkd->bhqd', attn, V)
-            context_final = self.layer_norm(self.w1(context) + self.w2(context_clustering))
+            context_final = self.layer_norm(context + self.w1(context_clustering))
 
             return context_final, attn, loss
 
