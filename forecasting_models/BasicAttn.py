79db142671c2b3e2b26553af02570e6d0a2d06d2
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-02-23 10:15:50 -0500

    adding clustering to all forecasting models

--- forecasting_models/BasicAttn.py
+++ forecasting_models/BasicAttn.py
@@ -41,6 +41,7 @@ class BasicAttn(nn.Module):
             return context_final, attn, loss
 
         else:
+
             scores = torch.einsum('bhqd,bhkd->bhqk', Q, K) / np.sqrt(self.d_k)
             attn = torch.softmax(scores, -1)
             context = torch.einsum('bhqk,bhkd->bhqd', attn, V)
