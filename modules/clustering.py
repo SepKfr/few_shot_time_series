3481e320a8c3d834bd4894f1638b0cffe5ac51cd
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-02-24 08:08:09 -0500

    3 clusters

--- modules/clustering.py
+++ modules/clustering.py
@@ -13,13 +13,13 @@ class Clustering(nn.Module):
         self.proj_back_to_cluster_k = nn.Sequential(nn.Linear(num_clusters, d_model, device=self.device),
                                                     nn.GELU())
 
-        self.cluster_k_proj = nn.Sequential(nn.Linear(d_model, num_clusters, device=self.device),
+        self.cluster_k_proj = nn.Sequential(nn.Linear(d_model, 4*num_clusters, device=self.device),
                                             nn.GELU(),
-                                            nn.Linear(num_clusters, num_clusters, device=self.device))
+                                            nn.Linear(4*num_clusters, num_clusters, device=self.device))
 
-        self.cluster_q_proj = nn.Sequential(nn.Linear(d_model, num_clusters, device=self.device),
+        self.cluster_q_proj = nn.Sequential(nn.Linear(d_model, 4*num_clusters, device=self.device),
                                             nn.GELU(),
-                                            nn.Linear(num_clusters, num_clusters, device=self.device))
+                                            nn.Linear(4*num_clusters, num_clusters, device=self.device))
 
         self.cross_entropy = nn.CrossEntropyLoss()
 
--- train.py
+++ train.py
@@ -237,11 +237,11 @@ class Train:
 def main():
 
     parser = argparse.ArgumentParser(description="preprocess argument parser")
-    parser.add_argument("--attn_type", type=str, default='autoformer')
-    parser.add_argument("--model_name", type=str, default="autoformer")
+    parser.add_argument("--attn_type", type=str, default='basic_attn')
+    parser.add_argument("--model_name", type=str, default="basic_attn")
     parser.add_argument("--exp_name", type=str, default='traffic')
     parser.add_argument("--cuda", type=str, default="cuda:0")
-    parser.add_argument("--seed", type=int, default=1899)
+    parser.add_argument("--seed", type=int, default=6949)
     parser.add_argument("--n_trials", type=int, default=50)
     parser.add_argument("--num_epochs", type=int, default=50)
     parser.add_argument("--few_shot", type=str, default="True")
